5e350c0311e300333dadc2d5d3281717ee9574b0
olesia8novoselova <nov-olesya.m@inbox.ru>
2025-02-11 00:41:17 +0300

    Added main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"short-links/handlers"
+	"short-links/storage"
+	"net/http"
+	"log"
+)
+
+func main() {
+	urlStorage := storage.NewMemoryStorage()
+	handler := handlers.NewHandler(urlStorage)
+
+	http.HandleFunc("/shorten", handler.ShortenURL)
+	http.HandleFunc("/original", handler.GetOriginalURL)
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
